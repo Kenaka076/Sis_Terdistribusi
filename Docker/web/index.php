91095b8b8e2ecaf38832f1d7c79afd4118aca1e1
Kenaka076 <127481253+Kenaka076@users.noreply.github.com>
2024-07-09 02:04:31 +0800

    Add files via upload

---
+++ Docker/web/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            background-color: black;
+            color: green;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo "Perkenalkan Nama saya Ida Bagus Gede Putra Kenaka Nim 2215101076";
+    ?>
+</body>
+</html>
